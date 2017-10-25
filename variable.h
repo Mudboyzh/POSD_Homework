0095cdd7166c1122001c492a780c2960227bb14c
MudboyZh <MudboyZh@MudboyZhdeMacBook-Pro.local>
2017-10-25 17:03:24 +0800

    test x.value()

--- variable.h
+++ variable.h
@@ -18,7 +18,10 @@ public:
 
 
   string value() const { 
-    if (_assignable && _value == NULL ) return _symbol;
+    if (_assignable && _value == NULL ) {
+      std::cout << "value case 1\n"; 
+      return _symbol;
+    }
     else if ( _assignable && _value != NULL) return _value->symbol();
     return _value->value(); 
   }
